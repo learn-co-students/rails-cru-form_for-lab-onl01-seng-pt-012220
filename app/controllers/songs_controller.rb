512c2a1112ac67d9c963f5b7ba33e26b17bafd8f
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-05-11 21:50:55 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,28 @@
+class SongsController < ApplicationController
+  def index 
+    
+  end
+  
+  def show
+    @song = Song.find_by(id: params[:id])
+  end
+  
+  def new
+    @song = Song.new
+  end 
+  
+  def create
+    @song = Song.create(params.require(:song).permit(:name, :artist_id, :genre_id))
+	  redirect_to song_path(@song)
+  end
+  
+  def update
+	  @song = Song.find(params[:id])
+	  @song.update(params.require(:song).permit(:name))
+	  redirect_to song_path(@song)
+	end
+
+	def edit
+	  @song = Song.find(params[:id])
+	end
+end
