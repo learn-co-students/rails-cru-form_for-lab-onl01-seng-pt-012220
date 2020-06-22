8c8aa33a1e226224bc764bcb4b71482de3fbd5ab
Jonathan <Jonnyghan@gmail.com>
2020-06-22 15:51:23 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,45 @@
+class SongsController < ApplicationController
+ 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    @artist = Artist.find(@song.artist_id)
+    @genre = Genre.find(@song.genre_id)
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+end
